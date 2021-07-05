d7eaecb4b40d8f93006beed29b340cbfff93c78c
Dmitry <wirwl@hotmail.com>
2021-07-05 19:31:48 +0600

    Add information if Descriminant is less than zero

--- src/blocks/App/App.tsx
+++ src/blocks/App/App.tsx
@@ -4,18 +4,23 @@ import QEPanel from "../QEPanel/QEPanel";
 import InputField from "../InputField/InputField";
 import { qeCalc } from "../../utils/utils";
 import { useState } from "react";
+import inf from './inf.svg';
 
 const b = block("App");
 
 const App = () => {
   const [root1, setRoot1] = useState<number>();
   const [root2, setRoot2] = useState<number>();
+  const [isShownInf, setIsShownInf] = useState<boolean>(false);
 
   const onCalc = (a: number, b: number, c: number) => {
     const roots = qeCalc(a, b, c);
     const [r1, r2] = roots;
+
     setRoot1(r1);
     setRoot2(r2);
+
+    setIsShownInf(roots.length === 0);
   }
 
   return (
@@ -39,6 +44,10 @@ const App = () => {
             <InputField inputValue={root2} isReadOnly={true} text="Корень 2" />
           </div>
         </div>
+      </div>
+      <div className={b('inf', { shown: isShownInf })}>
+        <img className={b('inf-image')} src={inf} alt='information' />
+        <span className={b('inf-text')}>Дискриминант отрицательное число, корней на множестве действительных чисел нет.</span>
       </div>
     </div>
   );
